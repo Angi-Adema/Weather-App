e345d3181b287a9487138a510f849387b6450fb6
CoderGirl <angi.adema@gmail.com>
2022-09-19 21:20:05 -0600

    updated .js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ var searchButton = document.getElementById('search-button');
 
 //Create a function that will send the request to the API for the data.
 function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
+    var requestUrl = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={aea21397c6d9dfc11ba04ff29f0547e6}'
 //Fetch request for the data.
     fetch(requestUrl)
         .then(function (response) {
